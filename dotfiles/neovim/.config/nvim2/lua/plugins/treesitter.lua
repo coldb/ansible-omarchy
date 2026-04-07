acbf01e711b15234fab525dcf868ddfd97ed29c3
Siim Lerg <siim.lerg@gmail.com>
2026-04-07 13:28:33 +0300

    chore: add treesitter and update plugins

--- dotfiles/neovim/.config/nvim2/lua/plugins/treesitter.lua
+++ dotfiles/neovim/.config/nvim2/lua/plugins/treesitter.lua
@@ -1,21 +1,10 @@
-return { -- Highlight, edit, and navigate code
+return {
   "nvim-treesitter/nvim-treesitter",
+  branch = "main",
   build = ":TSUpdate",
-  opts = {
-    ensure_installed = { "bash", "c", "diff", "html", "lua", "luadoc", "markdown", "vim", "vimdoc" },
-    -- Autoinstall languages that are not installed
-    auto_install = true,
-    highlight = {
-      enable = true,
-      -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-      --  If you are experiencing weird indenting issues, add the language to
-      --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-      additional_vim_regex_highlighting = { "ruby" },
-    },
-    indent = { enable = true, disable = { "ruby" } },
-  },
-  config = function(_, opts)
-    require("nvim-treesitter.install").prefer_git = true
-    require("nvim-treesitter").setup(opts)
+  lazy = false,
+  config = function()
+    require("nvim-treesitter").setup({})
+    require("nvim-treesitter").install({ "bash", "c", "diff", "html", "lua", "luadoc", "markdown", "vim", "vimdoc" })
   end,
 }
